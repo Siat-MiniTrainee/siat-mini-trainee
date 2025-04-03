008b7e4206a2a4dbe85101b067743d1e6216bfc0
정지훈 <mudokja@gmail.com>
2025-04-03 15:43:21 +0900

    feat : state 초기구현 플레이어 생성, 조회 구현, 아이템 서비스 연관기능 미완료 StateResponseDto 명칭 변경필요, 호환성을 위해 아직놔둠

--- state/controller/StateController.java
+++ state/controller/StateController.java
@@ -1,16 +1,25 @@
 package state.controller;
 
-import state.model.domain.StateDto;
+import java.util.List;
+
+import state.model.domain.StateResponseDto;
+import state.model.domain.StateUpdateInfoDto;
+import state.model.domain.StateUpdateRequestDto;
 import state.service.StateService;
 
 public class StateController {
     private StateService stateService = StateService.getInstance();
-
-    public StateDto getState() {
-        return stateService.getState();
+    public List<StateResponseDto> getPlayerList(){
+        return stateService.getPlayerList();
+    }
+    public StateResponseDto createPlayer(String playerName){
+        return stateService.createPlayer(playerName).orElse(StateResponseDto.builder().build());
+    }
+    public StateResponseDto getState(int playerId) {
+        return stateService.getState(playerId).orElse(StateResponseDto.builder().build());
     }
 
-    public void updateState(StateDto updatedState) {
-        stateService.updateState(updatedState);
+    public StateUpdateInfoDto updateState(StateUpdateRequestDto updatedState) {
+        return stateService.updateState(updatedState).orElse(StateUpdateInfoDto.builder().build());
     }
 }
